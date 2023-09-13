4c2106dce49ba934f41849a47e4c4104e160e222
Shaker055 <108206222+Shaker055@users.noreply.github.com>
2023-09-13 12:29:32 +0600

    Add files via upload

---
+++ banner .py
@@ -0,0 +1,10 @@
+#!/data/data/com.termux/files/usr/bin/bash
+clear
+echo -e "\033[1;33m"
+echo "  ____ _ _                 _"
+echo " / ___(_) | ___  _ __ __ _| |_ ___  _ __"
+echo "| |   | | |/ _ \| '__/ _\` | __/ _ \| '__|"
+echo "| |___| | | (_) | | | (_| | || (_) | |"
+echo " \____|_|_|\___/|_|  \__,_|\__\___/|_|"
+echo -e "\n\033[0mWelcome to Termux!"
+echo -e "Type 'help' to get started.\n"
